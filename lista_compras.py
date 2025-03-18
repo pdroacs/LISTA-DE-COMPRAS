aefc835e87e890ce760fd5009d666ea1c784c11b
Pedro Augusto <90412819+pdroacs@users.noreply.github.com>
2025-03-18 19:17:26 -0300

    Update lista_compras.py

--- lista_compras.py
+++ lista_compras.py
@@ -1,10 +1,3 @@
-"""
-Faça uma lista de comprar com listas
-O usuário deve ter a possibilidade de
-inserir, apagar e listar valores da sua lista
-Não permita que o programa quebre com
-erros de índices inexistentes na lista.
-"""
 import os
 
 lista = []
